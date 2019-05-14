a195c67ded4b949d1703dd77a1daae2b5a645189
UGUP <50613527+UGUP@users.noreply.github.com>
2019-05-14 13:02:15 +0530

    Update AssignmentFunctionsUpma.java

--- AssignmentFunctionsUpma.java
+++ AssignmentFunctionsUpma.java
@@ -1,7 +1,37 @@
 package com.company;
 
 public class AssignmentFunctionsUpma {
-    public static void main(String[] args){
-        
+    
+    public static void main(String[] args)
+    {
+        Scanner input1 = new Scanner(System.in);
+        int number1 = input1.nextInt();
+        int number2 = input1.nextInt();
+
+        int sum = 0;
+
+        sum = functions(number1,number2);
+
+        System.out.println("The sum of number "+number1+" and "+number2+" is "+sum);
+
+
     }
-}
+
+
+    public static int functions(int number1, int number2) {
+
+        int sum = 0;
+        sum = number1 + number2;
+
+        return (sum);
+
+
+    }
+        
+        
+}   
+        
+        
+        
+        
+    
