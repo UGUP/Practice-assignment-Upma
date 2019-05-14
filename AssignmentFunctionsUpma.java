29b1841c673b71a30375de67f5d59649543fe158
UGUP <50613527+UGUP@users.noreply.github.com>
2019-05-14 13:40:47 +0530

    Update AssignmentFunctionsUpma.java
    Here we added all the functions addition sub multiplication and division.

--- AssignmentFunctionsUpma.java
+++ AssignmentFunctionsUpma.java
@@ -4,34 +4,63 @@ public class AssignmentFunctionsUpma {
     
     public static void main(String[] args)
     {
+        
+        {
         Scanner input1 = new Scanner(System.in);
         int number1 = input1.nextInt();
         int number2 = input1.nextInt();
-
-        int sum = 0;
-
-        sum = functions(number1,number2);
-
-        System.out.println("The sum of number "+number1+" and "+number2+" is "+sum);
+        sum(number1,number2);
+        sub(number1,number2);
+        div(number1,number2);
+        mul(number1,number2);
 
 
     }
 
 
-    public static int functions(int number1, int number2) {
+       public static void sum(int number1,int number2)
 
-        int sum = 0;
-        sum = number1 + number2;
+        {
+            int sum = 0;
+            sum = number1 + number2;
 
-        return (sum);
+            System.out.println("The sum of number " + number1 + " and " + number2 + " is " + sum);
+
+        }
+
+    public static void div(int number1,int number2)
+
+    {     double div = 0;
+
+        div = (double)number1 / (double)number2;
+
+        System.out.println("The division of number "+number1+" and "+number2+" is "+div);
+    }
+
+
+    public static void mul(int number1,int number2)
+
+    {
+        int mul = 0;
+
+        mul = number1 * number2;
+        System.out.println("The multiplicatin of number "+number1+" and "+number2+" is "+mul);
+    }
+
+    public static void sub(int number1,int number2)
+
+    {
+        int sub = 0;
+
+
+        sub = Math.abs(number1 - number2);
+        System.out.println("The subtraction of number "+number1+" and "+number2+" is "+sub);
+
+    }
+
+
 
 
     }
+
         
-        
-}   
-        
-        
-        
-        
-    
