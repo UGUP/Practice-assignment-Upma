981aabfe7564d4a2cb8aaec1fae65de3754e839e
UGUP <50613527+UGUP@users.noreply.github.com>
2019-05-14 16:26:26 +0530

    Modified the code as suggested
    I removed the unnecessary return statements and also the parameters of the mainFunction along with declaration.

--- AssignmentFunctionsUpma.java
+++ AssignmentFunctionsUpma.java
@@ -2,26 +2,22 @@ package com.company;
 
 import java.util.*;
 
-public class AssignmentFunctionsUpma {
+public class Functions_ass {
 
 
     public static void main(String args[])
 
     {
-        //double number1=0.0,number2=0.0;//Declaration of variables
+        double a=mainFunction();//Calling the function after operation
 
-        //** I changed variable 'a' to 'result' and remove arguments from mainFunction()
-        //double a=mainFunction(number1,number2);
-        double result = mainFunction(); //Calling the function after operation
-
-
-        System.out.println("The return value is "+result);//Printing the output
+        System.out.println("The return value is "+a);//Printing the output
     }
 
-    public static double mainFunction() {///creating the main function which is calling all the operation functions, it accepts no arguments and returns result;
+    public static double mainFunction() {///creating the main function which is calling all the operation functions
 
         int choice = 0;
-        double number1, number2, result = 0;
+      double  number1 = 0.0;
+        double number2 = 0.0;
 
         System.out.println("Welcome to the calculator" + "\n" + "1.Addition" + "\n" + "2.Subtraction" + "\n" + "3.Multiplication" + "\n" + "4.Division" + "\n" +
                 "Enter your choice(1-4)");
@@ -34,53 +30,51 @@ public class AssignmentFunctionsUpma {
 
         number1 = input1.nextDouble();
         number2 = input1.nextDouble();
+        double result=0.0;
 
 
         switch (choice) {//Switch case to call each function based on the choice
             case 1:
                 result = sum(number1, number2);
-                //return (sum);//this is to return the function value to a single variable choice.
-                //** no need to return result for each case, you can do it once at the end of the function body.
-                break; //**use break after every case statement or else the following cases will also get invoked.
+                break;
+
 
             case 2:
                 result = sub(number1, number2);
-                //return (sub);
                 break;
 
 
             case 3:
-                result = mul(number1, number2);
-                //return (mul);
-                break;
+              result = mul(number1, number2);
+              break;
 
 
             case 4:
                 result = div(number1, number2);
-                //return (div);
-                break;
+               break;
 
 
 
             default:
                 System.out.println("Invalid option");
+                break;
 
         }
 
-        return result; //** sends back variable result from mainFunction() to main()
+        return result;
 
     }
 
 
 
-    public static double sum(double number1,double number2)//sum function created
+       public static double sum(double number1,double number2)//sum function created
 
-    {
-        double sum = 0;
-        sum = number1 + number2;
-        return(sum);
+        {
+            double sum = 0;
+            sum = number1 + number2;
+            return(sum);
 
-    }
+        }
 
     public static double div(double number1,double number2)//div function created
 
@@ -118,4 +112,4 @@ public class AssignmentFunctionsUpma {
 
 
 
-}
+    }
