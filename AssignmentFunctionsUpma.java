899b7bff9144a3a59303a6ad2bb3fd3eea3ef5f1
UGUP <50613527+UGUP@users.noreply.github.com>
2019-05-15 13:50:29 +0530

    Final submission with one exception not working :(
    Final submission with one exception not working :(

--- AssignmentFunctionsUpma.java
+++ AssignmentFunctionsUpma.java
@@ -6,29 +6,53 @@ public class Functions_ass {  //Creation of the Class
 
 
     public static void main(String args[]) {//Main Function
-        String choice2;//Declaration of variable to call the Calculator function again
+
+
+        String choice2 = "Y";
+        int choice;
+        double result;//Declaration of variable o call the Calculator function again
 
         do {//Loop to run the Calculator function again
 
-            System.out.println("Welcome to the calculator" + "\n" + "1.Addition" + "\n" + "2.Subtraction" + "\n" + "3.Multiplication" + "\n" + "4.Division" + "\n" +
-                    "Enter your choice(1-4)");
+            try {//handle exception for the choice 
+                System.out.println("Welcome to the calculator" + "\n" + "1.Addition" + "\n" + "2.Subtraction" + "\n" + "3.Multiplication" + "\n" + "4.Division" + "\n" +
+                        "Enter your choice(1-4)");
 
-            Scanner input1 = new Scanner(System.in);
+                Scanner input1 = new Scanner(System.in);
 
-            int choice = input1.nextInt();//Declaring the variable to input the operation to be performed
+                choice = input1.nextInt();//Declaring the variable to input the operation to be performed
 
-            double result = Calculator(choice);//Calling the calculator function
+                result = Calculator(choice);
+                //Calling the calculator function
 
-            System.out.println("The result value is " + result);
 
-            System.out.println("Thankyou Do you want to try again? Y/N");
-            choice2 = input1.next();
+                System.out.println("The result value is " + result);
+
+
+            } catch (Exception e) {//Exception handled
+                System.out.println("Enter only integer values for your Choice");
+
+            }
+
+
+try {//Exception not working 
+    System.out.println("Thank you Do you want to try again? Y/N");
+    Scanner input2 = new Scanner(System.in);
+    choice2 = input2.next();
+
+         }
+catch(Exception e)//not getting handled as of now 
+
+{
+    System.out.println("Enter correct value of choice2");
+}
         }
 
-        while(choice2.equals("Y"));//loop to run the calculator function again
 
 
-    }
+        while (choice2.equals("Y")) ;//loop to run the calculator function again
+
+        }
 
 
 
@@ -38,57 +62,57 @@ public class Functions_ass {  //Creation of the Class
     public static double Calculator(int choice) {
 
 
-        double number1=0.0,number2=0.0;
+        double number1 = 0.0, number2 = 0.0; double result = 0.0;
 
-       try {//Exception handling to handle the exception of invalid input of numbers
-           System.out.println("Enter the numbers");
-           Scanner input1 = new Scanner(System.in);
+        try {//Exception handling to handle the exception of invalid output for numbers
+            System.out.println("Enter the numbers");
+            Scanner input1 = new Scanner(System.in);
             number1 = input1.nextDouble();
             number2 = input1.nextDouble();
 
-       }
+           }
 
-       catch(Exception e )//exception handled
-
-       {
-           System.out.println("Please enter integer values");
-
-
-       }
-
-
-        double result = 0.0;
-        switch (choice) {//Switch case to call each function based on the choice
-            case 1:
-                result = number1 + number2;
-                break;
-
-
-            case 2:
-                result = number1 - number2;
-                break;
-
-
-            case 3:
-                result = number1 * number2;
-                break;
-
-
-            case 4:
-                result = number1 / number2;
-                break;
-
-
-            default:
-                System.out.println("Invalid option");
-                break;
-
+        catch(Exception e)
+        {
+            System.out.println("enter ony interger values for numbers");
         }
 
-        return result;
 
-    }
+                switch (choice) {//Switch case to call each function based on the choice
 
+
+                    case 1:
+                        result = number1 + number2;
+                        break;
+
+
+                    case 2:
+                        result = Math.abs(number1 - number2);
+                        break;
+
+
+                    case 3:
+                        result = number1 * number2;
+                        break;
+
+
+                    case 4:
+                        result = number1 / number2;
+                        break;
+
+
+                    default:
+                        System.out.println("Invalid option");
+                        break;
+
+                }
+
+
+            return result;
+
+            }
 }
+
+
 
 
